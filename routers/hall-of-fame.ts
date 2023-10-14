121451d216a9be9a56ee0997bb95b15a2dc57cbc
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-10-14 20:46:23 +0200

    Add views

--- routers/hall-of-fame.ts
+++ routers/hall-of-fame.ts
@@ -3,5 +3,5 @@ import { Router } from "express";
 export const hallOfFameRouter = Router();
 
 hallOfFameRouter.get("/", (req, res) => {
-  res.send("Lista najlepszych wojowników");
+  res.render("hall-of-fame/list");
 });
